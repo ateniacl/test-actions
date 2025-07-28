d8476234bbd21974e7821fce8b12343a9199bdbb
Asdrúbal Rafael Tenia Gonzalez <asdrubal.teniacl@gmail.com>
2025-07-27 20:02:28 -0400

    add

---
+++ HolaMundo.java
@@ -0,0 +1,5 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.print("hola estoy aprendiendo github actions" + " - " + "disparando primer trigger");
+    }
+}
