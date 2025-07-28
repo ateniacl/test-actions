5b72eae6e06941e7040e624bbf900fd62f53843e
Asdrúbal Tenia <asdrubal.tenia@gmail.com>
2025-07-27 20:16:25 -0400

    prueba2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.print("hola estoy aprendiendo github actions" + " | " + "disparando primer trigger");
+        System.out.print("hola estoy aprendiendo github actions" + " ||| " + "disparando primer trigger");
     }
 }
