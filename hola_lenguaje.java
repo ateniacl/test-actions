47ddcf0203be906e15028ff6e3f26afa825141d0
Asdrúbal Tenia <asdrubal.tenia@gmail.com>
2025-07-28 21:33:46 -0400

    hotfix

--- hola_lenguaje.java
+++ hola_lenguaje.java
@@ -1,5 +1,5 @@
 public class hola_lenguaje {
     public static void main(String[] args) {
-        System.out.print("hola estoy aprendiendo github actions" + " ||| " + "LENGUAJE");
+        System.out.print("hola estoy aprendiendo github actions" + " ||| " + "LENGUAJE XYZ");
     }
 }
